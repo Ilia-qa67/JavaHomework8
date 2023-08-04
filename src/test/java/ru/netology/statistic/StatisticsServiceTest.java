21a0b971fd2a182f8460f532803dd1f6cc2c2652
Ilia Tsarev <bkilja@gmail.com>
2023-08-04 19:25:42 +0400

    Reformat Code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -13,7 +13,7 @@ public class StatisticsServiceTest {
         long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
-        long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);git 
 
         Assertions.assertEquals(expected, actual);
     }
